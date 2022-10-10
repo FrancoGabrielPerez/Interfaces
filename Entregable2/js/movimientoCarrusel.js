d9967669dca1c39b7e6f4071a356994631c3001c
Franco Perez <FrancoGabrielPerez@gmail.com>
2022-10-10 03:22:50 -0300

    link a pag 2

--- Entregable2/js/movimientoCarrusel.js
+++ Entregable2/js/movimientoCarrusel.js
@@ -71,7 +71,7 @@ function hover(variable){
                     let auxURL = elemento.childNodes[1].childNodes[1].childNodes[5].childNodes[1].id;
                     elemento.childNodes[1].childNodes[1].childNodes[5].style.display = "none";
                     elemento.childNodes[1].childNodes[1].childNodes[7].style.display = "flex";
-                    elemento.childNodes[1].childNodes[1].childNodes[7].innerHTML = `<a href=http://127.0.0.1:5500/pages/${auxURL}.html><h1>Jugar</h1></a>`;
+                    elemento.childNodes[1].childNodes[1].childNodes[7].innerHTML = `<a href=./Connect_4.html><h1>Jugar</h1></a>`;
                     //console.log( elemento.childNodes[1].childNodes[1].childNodes[7].innerHTML);
                     }
                 else {
