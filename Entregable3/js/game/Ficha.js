9714bff474c7fd2757813d1c0f063546929ffd41
Franco Perez <FrancoGabrielPerez@gmail.com>
2022-10-29 20:20:54 -0300

    ficha

--- Entregable3/js/game/Ficha.js
+++ Entregable3/js/game/Ficha.js
@@ -4,29 +4,43 @@ class Ficha{
 	#centerX;
 	#centerY;
 	#radius;
+	#selected;
+	#ctx;
 
-	constructor(x,y,r){
-		this.centerX=x;
-		this.centerY=y;
-		this.radius=r;
+	constructor(x,y,r,contexto){
+		this.#centerX=x;
+		this.#centerY=y;
+		this.#radius=r;
+		this.#selected=false;
+		this.#ctx=contexto;
 	}
 
-	drawDefault(context){
-		this.drawFilled(context, 'red');
+	drawDefault(){
+		this.drawColor('red');
 	}
 
-	drawFilled(context, color){
-		context.beginPath();
-    	context.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI, false);
-    	context.fillStyle = color;
-    	context.fill();
-		context.stroke();
+	drawColor(color){
+		this.#ctx.beginPath();
+    	this.#ctx.arc(this.#centerX, this.#centerY, this.#radius, 0, 2 * Math.PI, false);
+    	this.#ctx.fillStyle = color;
+    	this.#ctx.fill();
+		this.#ctx.stroke();
 	}
 
 	isInside(posX, posY){
-		if (Math.sqrt(Math.pow(posX - this.centerX,2) + Math.pow(posY - this.centerY,2)) < this.radius)
-			return true;
-		else
-			return false;
+		return (Math.sqrt(Math.pow(posX - this.#centerX,2) + Math.pow(posY - this.#centerY,2)) < this.#radius);
 	}
+
+	isSelected(){
+        return this.selected;
+    }
+
+    setSelected(selected){
+        this.selected = selected;
+    }
+
+	move(x,y){
+        this.posX = x;
+        this.posY = y; 
+    }
 }
