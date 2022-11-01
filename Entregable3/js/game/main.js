0d1d9b45e5adeb5948b112836593cd7d424caa77
Franco Perez <FrancoGabrielPerez@gmail.com>
2022-11-01 03:33:00 -0300

    agregada logica de dibujado del tablero

--- Entregable3/js/game/main.js
+++ Entregable3/js/game/main.js
@@ -1,69 +1,57 @@
 "use strict"
+/** @type {CanvasRenderingContext2D} */
 
-let tablero = new Tablero(7, 6, 5);
-let ficha1 = new Ficha("J1",0,0,null);
-let ficha2 = new Ficha("J1",0,0,null);
-let ficha3 = new Ficha("J1",0,0,null);
-let ficha4 = new Ficha("J1",0,0,null);
-let ficha5 = new Ficha("J1",0,0,null);
-let ficha6 = new Ficha("J1",0,0,null);
-let ficha7 = new Ficha("J1",0,0,null);
-let ficha8 = new Ficha("J1",0,0,null);
-let ficha9 = new Ficha("J1",0,0,null);
-let ficha10 = new Ficha("J1",0,0,null);
-let ficha11 = new Ficha("J2",0,0,null);
-let ficha12 = new Ficha("J2",0,0,null);
-let ficha13 = new Ficha("J2",0,0,null);
-let ficha14 = new Ficha("J2",0,0,null);
-let ficha15 = new Ficha("J2",0,0,null);
-let ficha16 = new Ficha("J2",0,0,null);
-let ficha17 = new Ficha("J2",0,0,null);
-let ficha18 = new Ficha("J2",0,0,null);
-let ficha19 = new Ficha("J2",0,0,null);
-let ficha20 = new Ficha("J2",0,0,null);
+let canvas = document.querySelector("#canvas");
+let context = canvas.getContext('2d');
+
+let tablero = new Tablero(7, 6, 4, context, 50);
+let ficha1 = new Ficha("J1",0,0,context);
+let ficha2 = new Ficha("J1",0,0,context);
+let ficha3 = new Ficha("J1",0,0,context);
+let ficha4 = new Ficha("J1",0,0,context);
+let ficha5 = new Ficha("J1",0,0,context);
+let ficha6 = new Ficha("J1",0,0,context);
+let ficha7 = new Ficha("J1",0,0,context);
+let ficha8 = new Ficha("J1",0,0,context);
+let ficha9 = new Ficha("J1",0,0,context);
+let ficha10 = new Ficha("J1",0,0,context);
+let ficha11 = new Ficha("J2",0,0,context);
+let ficha12 = new Ficha("J2",0,0,context);
+let ficha13 = new Ficha("J2",0,0,context);
+let ficha14 = new Ficha("J2",0,0,context);
+let ficha15 = new Ficha("J2",0,0,context);
+let ficha16 = new Ficha("J2",0,0,context);
+let ficha17 = new Ficha("J2",0,0,context);
+let ficha18 = new Ficha("J2",0,0,context);
+let ficha19 = new Ficha("J2",0,0,context);
+let ficha20 = new Ficha("J2",0,0,context);
 
 tablero.printConsole();
+tablero.draw();
 
-/* tablero.agregarFicha(ficha1, 0);
-tablero.printConsole();
-tablero.agregarFicha(ficha11, 1);
-tablero.printConsole();
-tablero.agregarFicha(ficha2, 2);
-tablero.printConsole();
-tablero.agregarFicha(ficha12, 3);
-tablero.printConsole();
-tablero.agregarFicha(ficha3, 4);
-tablero.printConsole();
-tablero.agregarFicha(ficha13, 5);
-tablero.printConsole();
-tablero.agregarFicha(ficha4, 1);
-tablero.printConsole();
-tablero.agregarFicha(ficha14, 2);
-tablero.printConsole();
-tablero.agregarFicha(ficha15, 3);
-tablero.printConsole();
-tablero.agregarFicha(ficha16, 4);
-tablero.printConsole();
-tablero.agregarFicha(ficha17, 3);
-tablero.printConsole();
-tablero.agregarFicha(ficha5, 3);
-tablero.printConsole();
-tablero.agregarFicha(ficha17, 3);
-tablero.printConsole();
-tablero.agregarFicha(ficha6, 2);
-tablero.printConsole();
-tablero.agregarFicha(ficha18, 2);
-tablero.printConsole();
-tablero.agregarFicha(ficha7, 1);
-tablero.printConsole();
-tablero.agregarFicha(ficha8, 1);
-tablero.printConsole();
-tablero.agregarFicha(ficha19, 1);
-tablero.printConsole(); */
+// tablero.agregarFicha(ficha1, 0);
+// tablero.agregarFicha(ficha11, 3);
+// tablero.agregarFicha(ficha2, 0);
+// tablero.agregarFicha(ficha12, 0);
+// tablero.agregarFicha(ficha3, 1);
+// tablero.agregarFicha(ficha13, 0);
+// tablero.agregarFicha(ficha4, 1);
+// tablero.agregarFicha(ficha14, 1);
+// tablero.agregarFicha(ficha5, 3);
+// tablero.agregarFicha(ficha15, 2);
+// tablero.agregarFicha(ficha6, 4);
+// tablero.agregarFicha(ficha16, 2);
 
 function mostrarGanador(){
 	console.log("---------------------------");
 	console.log("Ganador");
 	console.log("jugador: " + tablero.getGanador());
 	tablero.printConsole();
+}
+
+function drawPoint(x, y,){
+	context.beginPath();
+	context.arc(x, y, 10, 0, 2 * Math.PI, false);
+	context.fillStyle = "green";
+	context.fill();
 }
