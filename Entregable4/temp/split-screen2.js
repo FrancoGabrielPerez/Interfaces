3f467ec07abf215ae90e55da3a605f9035a971a0
MarchioriRH <rubenmarchiori@gmail.com>
2022-11-18 22:30:41 -0300

    Animacion de history y cards

--- Entregable4/temp/split-screen2.js
+++ Entregable4/temp/split-screen2.js
@@ -3,35 +3,87 @@
 window.addEventListener(
   "scroll",
   () => {
-    console.log(window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
     let scrollPosition = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
-    if (scrollPosition >= 0.30  && scrollPosition < 0.40)
+    scrollPosition = scrollPosition.toFixed(2);
+    console.log(scrollPosition);
+
+    if (scrollPosition >= 0.30  && scrollPosition < 0.40){
         document.querySelector('#scrollLogo').setAttribute('src', "../img/img-games/hero-section/El Mono.png");
-    else if (scrollPosition >= 0.40)
+        if(scrollPosition == 0.30){
+          document.querySelector('.history-paragraph-2').classList.add('history-animation');
+          document.querySelector('#scrollLogo').classList.add('history-animation');
+        }
+          let animacion = document.querySelector('.history-paragraph-2');
+          animacion.addEventListener('animationend', () => {
+            document.querySelector(`.history-paragraph-2`).classList.remove('history-animation');
+            document.querySelector('#scrollLogo').classList.remove('history-animation');
+          });
+       
+    } else if (scrollPosition >= 0.40 && scrollPosition <= 0.54){
           document.querySelector('#scrollLogo').setAttribute('src', "../img/img-games/hero-section/Billy.png");
-        else      
-          document.querySelector('#scrollLogo').setAttribute('src', "../img/img-games/hero-section/El Juez.png");   
+          if(scrollPosition == 0.40){
+            document.querySelector('.history-paragraph-3').classList.add('history-animation');
+            document.querySelector('#scrollLogo').classList.add('history-animation');
+          }
+            let animacion = document.querySelector('.history-paragraph-3');
+            animacion.addEventListener('animationend', () => {
+              document.querySelector(`.history-paragraph-3`).classList.remove('history-animation');
+              document.querySelector('#scrollLogo').classList.remove('history-animation');
+            });
+          
+    } else {      
+          document.querySelector('#scrollLogo').setAttribute('src', "../img/img-games/hero-section/El Juez.png");
+          if(scrollPosition == 0.15){
+            document.querySelector('.history-paragraph-1').classList.add('history-animation');
+            document.querySelector('#scrollLogo').classList.add('history-animation');
+          }
+            let animacion = document.querySelector('.history-paragraph-1');
+            animacion.addEventListener('animationend', () => {document.querySelector(`.history-paragraph-1`).classList.remove('history-animation');
+            document.querySelector('#scrollLogo').classList.remove('history-animation');
+          });
+    }
+     
     
+
+    // Animacion cards de personajes
     let cards = document.querySelectorAll('.juego-character');
     let charactersChart = document.querySelector('.carrusel-characters');
-    if (scrollPosition >= 0.45  && scrollPosition < 0.80){
-      for(let i=0;i<9;i++){
+    if (scrollPosition >= 0.45  && scrollPosition < 0.60){
+      for(let i=1;i<5;i++){
         if (i % 2 == 0)
-          cards[i].classList.add('cards-movement-left');
+          cards[i].classList.add(`layer${i}`);
         else
-          cards[i].classList.add('cards-movement-right');
+          cards[i].classList.add(`layer${i}`);
       }
-      //charactersChart.classList.add('cards-movement');
       charactersChart.style = 'transform: translateX(0px)';
     } else {
-      for(let i=0;i<9;i++){
+      for(let i=1;i<5;i++){
         if (i % 2 == 0)
-          cards[i].classList.remove('cards-movement-left');
+          cards[i].classList.remove(`layer${i}`);
         else
-          cards[i].classList.remove('cards-movement-right');
+          cards[i].classList.remove(`layer${i}`);
       }
-      //charactersChart.classList.remove('cards-movement');
+    }
+    if (scrollPosition >= 0.61  && scrollPosition < 0.80){
+      for(let i=4;i<8;i++){
+        if (i % 2 == 0)
+          cards[i].classList.add(`layer${i+1}`);
+        else
+          cards[i].classList.add(`layer${i+1}`);
+      }
+      charactersChart.style = 'transform: translateX(0px)';
+    } else {
+      for(let i=4;i<8;i++){
+        if (i % 2 == 0)
+          cards[i].classList.remove(`layer${i+1}`);
+        else
+          cards[i].classList.remove(`layer${i+1}`);
+      }
     }
   },
   false
 );
+
+function animationRemove(i){
+      setTimeout(() => {document.querySelector(`.history-paragraph-${i}`).classList.remove('history-logo-animation');},5000 );
+}
