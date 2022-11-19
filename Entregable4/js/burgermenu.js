e1dea95765472b959e1e4d2f21c9d131ac4e14fe
Franco Perez <FrancoGabrielPerez@gmail.com>
2022-11-19 19:01:11 -0300

    menu desplegable con animacion

--- Entregable4/js/burgermenu.js
+++ Entregable4/js/burgermenu.js
@@ -1,10 +1,11 @@
 "use strict";
 document.querySelector('#nav-icon-left').addEventListener('click', hamButton);
-document.querySelector('#nav-icon-right').addEventListener('click', hamButton);
+//document.querySelector('#nav-icon-right').addEventListener('click', hamButton);
 
 /**ANIMACION BOTON HAMBURGESA*/
 function hamButton(e){
 	e.currentTarget.classList.toggle('open');
+	document.querySelector("#nav-menu").classList.toggle("hiddenn");
 }
 
 
