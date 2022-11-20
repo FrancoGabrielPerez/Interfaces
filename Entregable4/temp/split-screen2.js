6e741588d3a5e49c6ab46c7b34e21c86c808554b
MarchioriRH <rubenmarchiori@gmail.com>
2022-11-20 08:58:15 -0300

    Se continua probando animacion split screen

--- Entregable4/temp/split-screen2.js
+++ Entregable4/temp/split-screen2.js
@@ -4,55 +4,77 @@ window.addEventListener(
   "scroll",
   () => {
     let scrollPosition = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
+    let scrollPos = scrollPosition.toFixed(2);
     scrollPosition = scrollPosition.toFixed(3);
-    //console.log(scrollPosition);
-    console.log(window.pageYOffset );
+    console.log(scrollPosition);
+    /* console.log(window.pageYOffset );
+    let scrollHeight = Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight
+    );
+    console.log("Scrollheigth: "+scrollHeight); */
+    
 
     //Animacion de history section
+    if(scrollPosition < 0.300 || scrollPosition > 0.800){
+      document.querySelector('.history-paragraph-1').style = "opacity: 1";
+      document.querySelector('.history-paragraph-2').style = "opacity: 1";
+      document.querySelector('.history-paragraph-3').style = "opacity: 1";
+    }
+    
     if (scrollPosition >= 0.300  && scrollPosition < 0.400){
         document.querySelector('#scrollimg').setAttribute('src', "../img/img-games/hero-section/El Mono.png");
-        if(scrollPosition == 0.30){
+        
+        if(scrollPos == 0.35){
           document.querySelector('.history-paragraph-2').classList.add('history-paragraph-animation');
           document.querySelector('#scrollimg').classList.add('history-img-animation');
-          document.querySelector(`.history-paragraph-3`).style = "opcity: 0";
         }
-          let paragrapAnimation = document.querySelector('.history-paragraph-2');
+      
+        let paragrapAnimation = document.querySelector('.history-paragraph-2');
+        paragrapAnimation.addEventListener('animationend', () => {
+          document.querySelector(`.history-paragraph-2`).classList.remove('history-paragraph-animation');
+          document.querySelector(`.history-paragraph-2`).style = "opacity: 0";
+        });
+
+        let imgAnimation =  document.querySelector('#scrollimg');
+        imgAnimation.addEventListener('animationend', () => {
+          document.querySelector('#scrollimg').classList.remove('history-img-animation');
+        });
+
+       
+    } else if (scrollPosition >= 0.400 && scrollPosition <= 0.500){
+          document.querySelector('#scrollimg').setAttribute('src', "../img/img-games/hero-section/Billy.png");
+          if(scrollPos == 0.45){
+            document.querySelector('.history-paragraph-3').classList.add('history-paragraph-animation');
+            document.querySelector('#scrollimg').classList.add('history-img-animation');
+          }
+
+          let paragrapAnimation = document.querySelector('.history-paragraph-3');
           paragrapAnimation.addEventListener('animationend', () => {
-            document.querySelector(`.history-paragraph-2`).classList.remove('history-paragraph-animation');
+            document.querySelector(`.history-paragraph-3`).classList.remove('history-paragraph-animation');
+            document.querySelector(`.history-paragraph-3`).style = "opacity: 0";
           });
+
           let imgAnimation =  document.querySelector('#scrollimg');
           imgAnimation.addEventListener('animationend', () => {
             document.querySelector('#scrollimg').classList.remove('history-img-animation');
           });
-
-       
-    } else if (scrollPosition >= 0.400 && scrollPosition <= 0.540){
-          document.querySelector('#scrollimg').setAttribute('src', "../img/img-games/hero-section/Billy.png");
-          if(scrollPosition == 0.40){
-            document.querySelector('.history-paragraph-3').classList.add('history-paragraph-animation');
-            document.querySelector('#scrollimg').classList.add('history-img-animation');
-          }
-            let paragrapAnimation = document.querySelector('.history-paragraph-3');
-            paragrapAnimation.addEventListener('animationend', () => {
-              document.querySelector(`.history-paragraph-3`).classList.remove('history-paragraph-animation');
-              document.querySelector(`.history-paragraph-3`).style = "opcity: 0";
-            });
-            let imgAnimation =  document.querySelector('#scrollimg');
-            imgAnimation.addEventListener('animationend', () => {
-              document.querySelector('#scrollimg').classList.remove('history-img-animation');
-            });
           
-    } else if (scrollPosition >= 0.040 && scrollPosition <= 0.180){      
+    } else if (scrollPosition >= 0.100 && scrollPosition <= 0.300){      
           document.querySelector('#scrollimg').setAttribute('src', "../img/img-games/hero-section/El Juez.png");
-          if(scrollPosition == 0.15){
+          if(scrollPos == 0.15){
             document.querySelector('.history-paragraph-1').classList.add('history-paragraph-animation');
             document.querySelector('#scrollimg').classList.add('history-img-animation');
-            document.querySelector(`.history-paragraph-1`).style = "opcity: 0";
+            
           }
+
           let paragrapAnimation = document.querySelector('.history-paragraph-1');
           paragrapAnimation.addEventListener('animationend', () => {
             document.querySelector(`.history-paragraph-1`).classList.remove('history-paragraph-animation');
+            document.querySelector(`.history-paragraph-1`).style = "opacity: 0";
           });
+
           let imgAnimation =  document.querySelector('#scrollimg');
           imgAnimation.addEventListener('animationend', () => {
             document.querySelector('#scrollimg').classList.remove('history-img-animation');
