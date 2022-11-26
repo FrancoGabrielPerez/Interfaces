0b3998bd612402839aa7d634a6025bd907667ce0
MarchioriRH <rubenmarchiori@gmail.com>
2022-11-26 20:51:14 -0300

    Ruben 26nov 2100hs

--- Entregable4/js/split-screen.js
+++ Entregable4/js/split-screen.js
@@ -61,61 +61,6 @@ window.addEventListener(
     //   console.log(titleDiv1);
 
     // }
-    /* function heroTitleAnimation(){
-      let title = document.querySelector(".hero-title");
-      let posTitle = title.getBoundingClientRect().top + title.clientHeight/2;
-      let initialPosTitle = posTitle;
-      let relativepos =(window.innerHeight/2-posTitle)/window.innerHeight;
-    
-      console.log("titlescrollup ",titleScrollUp); 
-      console.log("titlescrolldown ",titleScrollDown); 
-      console.log("relativapos ",relativepos);    
-      if (scrollDirection == 'down' && titleScrollDown < 400){
-        title.style = `transform: translateY(${titleScrollDown}px); opacity: ${1-(titleScrollDown/400)};`;        
-        console.log(titleScrollDown/400);
-        titleScrollDown += 15;
-        titleScrollUp += 15;
-      } 
-      if (scrollDirection == 'up' && titleScrollDown > 0){
-        title.style = `transform: translateY(${titleScrollDown}px); opacity: ${1-(titleScrollDown/400)};`;         
-        console.log("opacity "+titleScrollDown/400);
-        titleScrollUp -= 15;
-        titleScrollDown -= 15;
-      } 
-    }
-
-    heroTitleAnimation();
- */
-    // Animacion cards de personajes.
-   
-    // let cards = document.querySelectorAll('.juego-character');
-    // let charactersChart = document.querySelector('.carrusel-characters');
-  
-    // if (scrollPosition <= 0.50 || scrollPosition >= 0.77){
-    //   cards.forEach(card => {card.style = 'opacity: 0';});
-    // }
-
-    // if (scrollPos == 0.60 /*  && scrollPosition < 0.650  */){
-    //   for(let i = 0; i < 4; i++){
-    //     if (i % 2 != 0)
-    //       cards[i].classList.add(`layer${i+1}`);
-    //     else
-    //       cards[i].classList.add(`layer${i+1}`);
-    //   }
-    // }    
-    // if (scrollPosition == 0.70 /* && scrollPosition < 0.750 */){
-    //   for(let i = 4; i < 8; i++){
-        
-    //     if (i % 2 != 0)
-    //       cards[i].classList.add(`layer${i+1}`);
-    //     else
-    //       cards[i].classList.add(`layer${i+1}`);
-    //   }
-    // } 
-    // cards.forEach(card => card.addEventListener('animationend', () =>{
-    //   card.classList = 'juego-character';
-    //   card.style = 'opacity: 1';
-    // }));
     
   },
   false
