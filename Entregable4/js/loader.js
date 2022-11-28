8020f47dfa47cc25e5fffcf9c1cf0bf08ae3bce5
Franco Perez <FrancoGabrielPerez@gmail.com>
2022-11-28 17:22:40 -0300

    forget scroll pos

--- Entregable4/js/loader.js
+++ Entregable4/js/loader.js
@@ -1,7 +1,12 @@
 "use strict";
 
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+  }
+
 window.scrollTo( 0, 0);
 document.querySelector("header").classList.add("hidden");
+
 setTimeout(()=>{   
     document.querySelector("header").classList.remove("hidden");
     document.body.style = "overflow-y:scroll;"; 
