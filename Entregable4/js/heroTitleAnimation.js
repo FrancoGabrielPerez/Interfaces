eacc38807f0220804cc267a7550df806e8370593
Franco Perez <FrancoGabrielPerez@gmail.com>
2022-11-28 21:50:10 -0300

    fix title

--- Entregable4/js/heroTitleAnimation.js
+++ Entregable4/js/heroTitleAnimation.js
@@ -1,17 +1,14 @@
 'use strict';
 
 let title = document.querySelector('.hero-title');
-let cowBones = document.querySelector('.cow-bones')
 window.addEventListener("scroll", heroTitleAnimation);
 
 function heroTitleAnimation(){
-  const trigger = window.innerHeight/6;
-  var top = cowBones.getBoundingClientRect().top;
-  top = top + top * 0.5; 
-  if (trigger > top){
-      title.classList.add('hide-title');
+   let scroll = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
+  if (scroll <= 0){
+     title.classList.remove('hide-title');
    } else {    
-      title.classList.remove('hide-title');
+      title.classList.add('hide-title');
    }
 }
 
